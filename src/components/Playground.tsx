42bb2f5ef66605974f5671c8f5ccb4607c726417
Nikas Praninskas <nikaspran@gmail.com>
2024-12-08 23:20:52 +0200

    Add basic configuration options

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -1,5 +1,6 @@
 "use client";
 import nunjucks from "nunjucks/browser/nunjucks";
+import set from "lodash/set";
 import useLocalStorage from "@/hooks/useLocalStorage";
 import jsYaml from "js-yaml";
 import { Editor } from "./Editor";
@@ -42,6 +43,39 @@ function CardHeader({
 
 const DATA_FORMATS = ["yaml", "json"] as const;
 
+const DEFAULT_OPTIONS = {
+  autoescape: true,
+  throwOnUndefined: false,
+  trimBlocks: false,
+  lstripBlocks: false,
+  tags: {
+    blockStart: "{%",
+    blockEnd: "%}",
+    variableStart: "{{",
+    variableEnd: "}}",
+    commentStart: "{#",
+    commentEnd: "#}",
+  },
+} as nunjucks.ConfigureOptions;
+
+function parseOptions(serialized: string) {
+  const parsed = JSON.parse(serialized);
+  return {
+    autoescape: parsed.autoescape,
+    throwOnUndefined: parsed.throwOnUndefined,
+    trimBlocks: parsed.trimBlocks,
+    lstripBlocks: parsed.lstripBlocks,
+    tags: {
+      blockStart: parsed.tags?.blockStart,
+      blockEnd: parsed.tags?.blockEnd,
+      variableStart: parsed.tags?.variableStart,
+      variableEnd: parsed.tags?.variableEnd,
+      commentStart: parsed.tags?.commentStart,
+      commentEnd: parsed.tags?.commentEnd,
+    },
+  } as nunjucks.ConfigureOptions;
+}
+
 export default function Playground({
   language,
 }: {
@@ -60,9 +94,21 @@ export default function Playground({
     `data-${language}-${selectedDataFormat}`,
     "" as string
   );
+  const [options, setOptions] = useLocalStorage(
+    `options-${language}`,
+    JSON.stringify(DEFAULT_OPTIONS)
+  );
+
+  const parsedOptions = options ? parseOptions(options) : DEFAULT_OPTIONS;
+
+  function setOption(path: string, value: any) {
+    const copy = JSON.parse(JSON.stringify(parsedOptions));
+    set(copy, path, value);
+    setOptions(JSON.stringify(copy));
+  }
 
   useEffect(() => {
-    const env = new nunjucks.Environment(new NunjucksLoader(), {});
+    const env = new nunjucks.Environment(new NunjucksLoader(), parsedOptions);
     try {
       const parsedData = data
         ? selectedDataFormat === "json"
@@ -76,7 +122,7 @@ export default function Playground({
     } catch (error: any) {
       setOutput(error.toString());
     }
-  }, [debouncedTemplate, language, data, selectedDataFormat]);
+  }, [debouncedTemplate, language, data, selectedDataFormat, options]);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
@@ -138,7 +184,61 @@ export default function Playground({
       <Card>
         <CardHeader>Options</CardHeader>
 
-        <div className="min-h-[300px]">Some test content</div>
+        <div className="min-h-[300px] py-2 px-4">
+          <div className="grid grid-cols-2 gap-1">
+            {(
+              [
+                "autoescape",
+                "throwOnUndefined",
+                "trimBlocks",
+                "lstripBlocks",
+              ] as const
+            ).map((booleanOption) => (
+              <div key={booleanOption}>
+                <input
+                  type="checkbox"
+                  className="cursor-pointer"
+                  id={booleanOption}
+                  checked={parsedOptions[booleanOption]}
+                  onChange={(event) =>
+                    setOption(booleanOption, event.target.value)
+                  }
+                />
+                <label className="pl-1 cursor-pointer" htmlFor={booleanOption}>
+                  {booleanOption}
+                </label>
+              </div>
+            ))}
+          </div>
+
+          <div className="grid grid-cols-2 gap-1 pt-2">
+            {(
+              [
+                "blockStart",
+                "blockEnd",
+                "variableStart",
+                "variableEnd",
+                "commentStart",
+                "commentEnd",
+              ] as const
+            ).map((tagOption) => (
+              <div key={tagOption}>
+                <input
+                  type="text"
+                  className="text-slate-800"
+                  id={tagOption}
+                  value={parsedOptions?.tags?.[tagOption]}
+                  onChange={(event) =>
+                    setOption(`tags.${tagOption}`, event.target.value)
+                  }
+                />
+                <label className="pl-1 cursor-pointer" htmlFor={tagOption}>
+                  {tagOption}
+                </label>
+              </div>
+            ))}
+          </div>
+        </div>
       </Card>
     </div>
   );
